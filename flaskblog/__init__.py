518ff8843abfba83b85a3051fb866e2c324ef10a
Rshivapu <rohitshivapur18@gmail.com>
2018-12-14 13:14:39 -0800

    Adding username and login validations

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 import datetime
 
@@ -8,5 +10,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '231cc16d13eb5311d1e568b3de141393'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
